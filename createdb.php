1ae7338936aa2856de92b1fe77fe82b04c1cd465
Ztaylor5 <158499448+Ztaylor5@users.noreply.github.com>
2024-02-09 20:46:14 -0600

    Add files via upload

---
+++ createdb.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <meta charset = "utf-8">
+    <head>
+        <body>
+            <?php
+            //replace the user name and password with your credentials
+            $host = "theipandportnumberhere";
+            #user = "ztaylor5";
+            $password = "Jinisb34n$"
+            $db = "visitordb";
+            // connect to the database
+            $conn = mysqli_init();
+            mysqli_real_connect($conn, $host, $user, $password, $db, 3306);
+            $query = "CREAT TABLE visitor
+            (
+                visitorid INTEGER AUTO_INCREMENT,
+                visitorName VARCHAR(100) NOT NULL,
+                visitTime TIMESTAMP DEFAULT NOW(),
+                PRIMARY KEY(visitorid)
+                )";
+                if (mysqli_query($conn, $query))
+                echo "<p>Table created.</p>";
+            //close connection
+            mysqli_close($conn);
+            ?>
+        </body>
+    </head>
